301264a37ca010d9a6ada5f7d222b1400c020e3a
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-02-26 14:51:26 +0700

    Updates.

--- resources/js/components/DokumenShow.vue
+++ resources/js/components/DokumenShow.vue
@@ -5,22 +5,66 @@
     </div>
 
     <div class="card-body">
+      <table class="table table-sm table-striped">
+        <thead>
+        <tr>
+          <th> # </th>
+          <th> Teks </th>
+          <th class="text-center"> Miringkan? </th>
+        </tr>
+        </thead>
 
+        <tbody>
 
+        <tr
+            v-for="(correction, key) in corrections"
+            :key="key"
+        >
+          <td> {{ key + 1 }}</td>
+          <td> {{ correction.node.textContent }}</td>
+          <td class="d-flex justify-content-center">
+            <div class="form-check">
+              <input
+                  id="flexCheckChecked"
+                  class="form-check-input"
+                  type="checkbox"
+                  v-model="correction.applies"
+              >
+              <label class="form-check-label sr-only"
+                     for="flexCheckChecked"
+              >
+                Lakukan Koreksi Untuk {{ correction.node.textContent }}, item ke {{ key + 1 }}
+              </label>
+            </div>
+
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="card-footer d-flex justify-content-end">
+      <button @click="onSubmit" class="btn btn-primary">
+        Lakukan Revisi
+      </button>
     </div>
   </div>
 
 
-  <div class="card" v-if="document">
+  <div v-if="document"
+       class="card"
+  >
     <div class="card-header">
       {{ document.nama }}
     </div>
 
     <div class="card-body">
       <iframe
-          id="iframe"
+          id="documentFrame"
+          ref="documentFrame"
           :srcdoc="document.html"
           class="document"
+          @load="onDocumentLoad"
       ></iframe>
     </div>
   </div>
@@ -29,6 +73,7 @@
 <script>
 
 import axios from "axios"
+import {onMounted, ref, computed} from "vue"
 
 export default {
   name: "DokumenShow",
@@ -38,56 +83,60 @@ export default {
     "reviseActionUrl": String,
   },
 
-  data() {
-    return {
-      document: null,
-      corrections: [],
+  setup(props) {
+    const document = ref(null)
+    const documentFrame = ref(null)
+    const corrections = ref([])
+    const formData = computed(() => corrections.value.map(correction => ({
+      id: correction.node.id,
+    })))
+
+    const onSubmit = () => {
+      // TODO Implement this thing
+    };
+
+    onMounted(async () => {
+      let response = await axios.get(props.dataSourceUrl)
+      document.value = response.data
+    })
+
+    const markDocumentParts = contentDocument => {
+      let head = contentDocument.querySelector("head")
+      let styleElement = contentDocument.createElement('style')
+      styleElement.innerHTML = "span.marked { text-decoration: underline; text-decoration-color: red; text-decoration-style: wavy }"
+      head.appendChild(styleElement)
     }
-  },
 
-  methods: {
-    loadStyles: function () {
-      window.setTimeout(() => {
-        let iframe = document.getElementById("iframe")
-        let head = iframe.contentDocument.getElementsByTagName("head")[0]
+    const onDocumentLoad = e => {
+      markDocumentParts(e.target.contentDocument)
 
-        let styleElement = iframe.contentDocument.createElement('style')
-        styleElement.innerHTML = "span.marked { text-decoration: underline; text-decoration-color: red; text-decoration-style: wavy }"
-
-        head.appendChild(styleElement)
-      }, 500)
-    },
-  },
-
-  watch: {
-    "document.html": function (newDocumentHtml) {
-        if (newDocumentHtml === null) {
-          return
-        }
-
-        this.loadStyles();
-    }
-  },
-
-  mounted() {
-    axios.get(this.dataSourceUrl)
-        .then(response => {
-          this.document = response.data
+      let contentDocument = e.target.contentDocument
+      contentDocument.querySelectorAll("span.marked").forEach(elem => {
+        corrections.value.push({
+          node: elem,
+          applies: true,
         })
+      })
+    }
+
+    return {
+      document,
+      documentFrame,
+      onDocumentLoad,
+      corrections,
+      formData,
+      onSubmit,
+    }
   }
 }
 </script>
 
 <style scoped>
-span.marked {
-  text-decoration: underline;
-  text-decoration-color: red;
-  text-decoration-style: wavy;
-}
 
 iframe.document {
   width: 100%;
   height: 640px;
   overflow-y: scroll;
 }
+
 </style>
